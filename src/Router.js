50c6faf4f923aeba15cfbaf2ba578318fed96f4e
JoaoVit0r <joao_cunha_16@outlook.com>
2022-10-26 20:27:12 -0300

    trying with different route

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,12 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 import { Home } from "./pages/Home";
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<h1>Page Not Found: 404</h1>} />
+      <Route path="/home" element={<Home />} />
+      <Route path="*" element={<Navigate to="/home"/>} />
     </Routes>
   );
 }
